fe1e86a31569c6a8686d01cc279e5bdf825825d4
root27 <oguzhaneee@gmail.com>
2024-02-16 15:22:25 +0300

    update fix

--- handlers/update.go
+++ handlers/update.go
@@ -17,6 +17,16 @@ func Update(c *fiber.Ctx, db *sql.DB) error {
 
 	var data Data
 
+	e := c.BodyParser(&data)
+
+	if e != nil {
+		c.JSON(fiber.Map{
+			"message": "Error parsing request",
+		})
+
+		return e
+	}
+
 	_, err := db.Query("UPDATE users SET name = $1, age = $2 WHERE id = $3", data.Name, data.Age, id)
 
 	if err != nil {
